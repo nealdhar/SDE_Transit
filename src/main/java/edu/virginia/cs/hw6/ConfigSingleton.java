be1a2c23bd692c926fbd7416c799d711925d64c4
David Le <uyp7dr@virginia.edu>
2023-04-12 22:26:40 -0400

    Fixed pt. 1 to read from the config file

--- src/main/java/edu/virginia/cs/hw6/ConfigSingleton.java
+++ src/main/java/edu/virginia/cs/hw6/ConfigSingleton.java
@@ -1,5 +1,9 @@
 package edu.virginia.cs.hw6;
 
+import org.json.JSONObject;
+
+import java.io.*;
+
 public class ConfigSingleton {
     private static final String configurationFileName = "config.json";
     private static ConfigSingleton instance;
@@ -32,8 +36,18 @@ public class ConfigSingleton {
 
     private void setFieldsFromJSON() {
         //TODO: Population the three fields from the config.json file
-        busStopsURL = "https://api.devhub.virginia.edu/v1/transit/bus-stops" ;
-        busLinesURL = "https://api.devhub.virginia.edu/v1/transit/routes" ;
-        databaseName = "bus_stops.sqlite3" ;
+        String filePath = "edu.virginia.cs.hw6/" + configurationFileName;
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(filePath);
+        try {
+            String jsonString = new String(inputStream.readAllBytes());
+            JSONObject jsonObject = new JSONObject(jsonString);
+            JSONObject endpoints = jsonObject.getJSONObject("endpoints");
+            busStopsURL = endpoints.getString("stops");
+            busLinesURL = endpoints.getString("lines");
+            databaseName = jsonObject.getString("database");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
