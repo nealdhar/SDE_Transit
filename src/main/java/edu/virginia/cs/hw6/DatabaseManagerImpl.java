2a4951c49dc4401f7ad8bbf5bfe95005e6a40e37
oliviafountain <ogf9uhy@virginia.edu>
2023-04-16 00:21:31 -0400

    Completing createTables() function

--- src/main/java/edu/virginia/cs/hw6/DatabaseManagerImpl.java
+++ src/main/java/edu/virginia/cs/hw6/DatabaseManagerImpl.java
@@ -1,17 +1,54 @@
 package edu.virginia.cs.hw6;
 
+import java.sql.SQLException;
 import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class DatabaseManagerImpl implements DatabaseManager {
 
+    Connection connection;
     @Override
     public void connect() {
-
+        String databaseURL = "jdbc:sqlite:bus_stops.sqlite3";
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection(databaseURL);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void createTables() {
+        String createStopsTable = "CREATE TABLE IF NOT EXISTS " +
+                "Stops (ID INTEGER PRIMARY KEY, " +
+                "Name VARCHAR(255) NOT NULL, " +
+                "Latitude DOUBLE NOT NULL, " +
+                "Longitude DOUBLE NOT NULL);";
 
+        String createBusLinesTable = "CREATE TABLE IF NOT EXISTS " +
+                "BusLines (ID INTEGER PRIMARY KEY, " +
+                "IsActive BOOLEAN NOT NULL, " +
+                "LongName VARCHAR(255) NOT NULL, " +
+                "ShortName VARCHAR(255) NOT NULL);";
+
+        String createRoutesTable = "CREATE TABLE IF NOT EXISTS " +
+                "Routes (ID INTEGER AUTO_INCREMENT PRIMARY KEY, " +
+                "BusLineID INTEGER NOT NULL, " +
+                "FOREIGN KEY (BusLineID) REFERENCES BusLines(ID), " +
+                "StopID INTEGER NOT NULL, " +
+                "FOREIGN KEY (StopID) REFERENCES Stops(ID), " +
+                "Order INTEGER NOT NULL);";
+
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(createStopsTable);
+            statement.executeUpdate(createBusLinesTable);
+            statement.executeUpdate(createRoutesTable);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
